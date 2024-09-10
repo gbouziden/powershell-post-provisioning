e395cba52bea03caf667d8806d8895bd6d6871bb
George Bouziden <george.bouziden@gmail.com>
2024-09-10 12:47:25 -0500

    Update groupMembershipJoin.ps1
    Update with function

--- groupMembershipJoin.ps1
+++ groupMembershipJoin.ps1
@@ -1,4 +1,4 @@
-# This uses a default gateway to apply group memebership to teams
+# This uses a default gateway to apply group membership to teams
 # Switch's can be added to expand to more teams and cases can be
 # added to run more provisioning steps
 
@@ -10,6 +10,42 @@ $credential = New-Object System.Management.Automation.PSCredential ($username, $
 $computerName = $env:COMPUTERNAME
 get-windowsfeature | where name -like RSAT-AD-PowerShell | Install-WindowsFeature
 
+# Function to handle common tasks
+function Handle-Computer {
+    param (
+        [string]$computerName,
+        [pscredential]$credential
+    )
+
+    # Check if the computer object exists in AD
+    $adComputer = New-PSSession -ComputerName $computerName -Credential $credential; Get-ADComputer -Identity $computerName -ErrorAction SilentlyContinue; Remove-PSSession -Session $session
+
+    if ($adComputer) {
+        write-host("Computer object exists in AD. Removing it.")
+        
+        # Enter a remote PowerShell session
+        $session = New-PSSession -ComputerName $computerName -Credential $credential
+        
+        Invoke-Command -Session $session -ScriptBlock {
+            param ($credential)
+            
+            # Remove the computer object from AD
+            Remove-ADComputer -Identity $computerName -Credential $credential -Confirm:$false -Force
+            
+            # Leave the domain
+            Remove-Computer -UnjoinDomainCredential $credential -PassThru -Verbose -Force #-Restart
+        } -ArgumentList $credential
+        
+        # Close the remote session
+        Remove-PSSession -Session $session
+    }
+    
+    # Add the computer to the domain
+    Add-Computer -DomainName domain.youplace.com -OUPath "OU=Path,dc=domain,dc=yourplace,dc=com" -ErrorAction stop -Credential $credential -Force
+    Add-LocalGroupMember -Group "Administrators" -Member "DOMAIN\group_name"
+    Restart-Computer -Force
+}
+
 $defaultGateways = Get-NetRoute |
     where {$_.DestinationPrefix -eq '0.0.0.0/0'} |
     Select-Object -ExpandProperty NextHop
@@ -18,42 +54,15 @@ foreach ($defaultGateway in $defaultGateways) {
     switch ($defaultGateway) {
         "10.1.100.1" {
             write-host("The Gateway is 10.1.100.1")
-            #run join command for this tenant
+            # Run join command for this tenant
+            Handle-Computer -computerName $computerName -credential $credential
         }
         "10.1.101.1" {
             write-host("The Gateway is 10.1.101.1")
-            # Check if the computer object exists in AD
-            # You can remove these checks if you already have a good process to check for duplicate hostnames
-            $computerName = $env:COMPUTERNAME
-            $adComputer = New-PSSession -ComputerName $computerName -Credential $credential; Get-ADComputer -Identity $computerName -ErrorAction SilentlyContinue; Remove-PSSession -Session $session
-          
-            if ($adComputer) {
-                write-host("Computer object exists in AD. Removing it.")
-                
-                # Enter a remote PowerShell session
-                $session = New-PSSession -ComputerName $computerName -Credential $credential
-                
-                Invoke-Command -Session $session -ScriptBlock {
-                    param ($credential)
-                    
-                    # Remove the computer object from AD
-                    Remove-ADComputer -Identity $computerName -Credential $credential -Confirm:$false -Force
-                    
-                    # Leave the domain
-                    Remove-Computer -UnjoinDomainCredential $credential -PassThru -Verbose -Force #-Restart
-                } -ArgumentList $credential
-                
-                # Close the remote session
-                Remove-PSSession -Session $session
-            }
-            
-            # Add the computer to the domain
-            Add-Computer -DomainName domain.youplace.com -OUPath "OU=Path,dc=domain,dc=yourplace,dc=com" -ErrorAction stop -Credential $credential -Force
-            Add-LocalGroupMember -Group "Administrators" -Member "DOMAIN\group_name"
-            Restart-Computer -Force
+            Handle-Computer -computerName $computerName -credential $credential
         }
         default {
-            write-host("no gw")
+            write-host("No gateway matched")
         }
     }
-} 
+}
